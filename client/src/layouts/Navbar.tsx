60b5f09d1be45d7337c200956e3d887b1500fea7
kaan-ozer <kaanozer35@gmail.com>
2025-02-22 20:01:42 +0300

    feature/frontend/curriculum-edit
    -User icon change

--- client/src/layouts/Navbar.tsx
+++ client/src/layouts/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "../components/LanguageSwitcher";
+import { BsPersonCircle } from "react-icons/bs";
 
 function Navbar() {
   const { t } = useTranslation();
@@ -32,9 +33,7 @@ function Navbar() {
             <LanguageSwitcher />
             <button className="p-2 rounded-full hover:bg-gray-100">
               <span className="sr-only">{t('common.userMenu')}</span>
-              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-              </svg>
+              <BsPersonCircle size={35} />
             </button>
           </div>
         </div>
