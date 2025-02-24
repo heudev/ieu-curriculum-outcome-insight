35732a7397a49cb8eb68ba6fafa8e54a3b8393a0
kaan-ozer <kaanozer35@gmail.com>
2025-02-24 23:52:37 +0300

    feature/frontend/curriculum-edit
    - navigation helper has been added

--- server/v1/src/scripts/utils/helper.js
+++ server/v1/src/scripts/utils/helper.js
@@ -1,5 +1,8 @@
 const CryptoJS = require("crypto-js");
 const JWT = require("jsonwebtoken");
+const path = require('path');
+
+module.exports = path.dirname(require.main.filename);
 
 const passwordToHash = (password) => {
     return CryptoJS.HmacSHA256(password, CryptoJS.HmacSHA1(password, process.env.PASSWORD_HASH).toString()).toString();
