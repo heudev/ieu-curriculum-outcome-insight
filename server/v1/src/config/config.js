46a0f9408c09c6fadb3a0f3e19048670c51bc97c
kaan-ozer <kaanozer35@gmail.com>
2025-02-25 00:08:39 +0300

    feature/frontend/curriculum-edit
    - config.js has been updated

--- server/v1/src/config/config.js
+++ server/v1/src/config/config.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: process.env.DB_DIALECT || "postgres",
     migrations: "v1/src/migrations",
   },
 };
