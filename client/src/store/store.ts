f5d96b9a32d88d20f42c4076e16bd4b15fa6c554
kaan-ozer <kaanozer35@gmail.com>
2025-02-27 00:51:16 +0300

    feature/frontend/curriculum-edit
    -Reducer,effects and store have been initialized for form model.

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import courseReducer from './slices/courseSlice'
+import formReducer from "./slices/formSlice";
 
 export const store = configureStore({
   reducer: {
     course: courseReducer,
+    forms: formReducer,
   },
 })
 
